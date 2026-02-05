bc10ed523142ea94e4cf43b8187ac6c8c2f68bbd
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-05 17:56:05 +0600

    Add create pulse routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\PulseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PulseController::class, 'index']);
+Route::get('/pulses/create', [PulseController::class, 'create'])->name('pulse.create');
+Route::post('/pulses', [PulseController::class, 'store'])->name('pulse.store');
