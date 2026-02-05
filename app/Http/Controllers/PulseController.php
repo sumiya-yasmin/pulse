84325afb14867b21a33b53422efe3f73d6166229
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-05 17:55:51 +0600

    Add create pulse controller

--- app/Http/Controllers/PulseController.php
+++ app/Http/Controllers/PulseController.php
@@ -13,4 +13,22 @@ class PulseController extends Controller
 
         return view('home', ['pulses' => $pulses]);
     }
+
+    public function create()
+    {
+        return view('pulses.create');
+    }
+    public function store(Request $request)
+    {
+       $data = $request->validate([
+        'title' => 'required|min:3|max:255',
+        'body' => 'required|min:10',
+        'mood' => 'required|min:50',
+        'energy' => 'required|integer|min:1|max:10'
+
+       ]);
+       \App\Models\Pulse::create($data + ['user_id'=>1]);
+       return redirect('/')->with('success', 'Pulse saved!');
+    }
+
 }
