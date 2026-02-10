405113a33d2dabfcb6fd61feb1f5cc53cdd0878e
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-10 16:31:10 +0600

    Add individual pulse show page

--- resources/views/pulses/show.blade.php
+++ resources/views/pulses/show.blade.php
@@ -1,8 +1,14 @@
 <x-layout>
     <x-slot:title>
-        - {{$pulse->title}}
-</x-slot:title>
-<div>
-    <x-pulse-card :pulse="$pulse" />
-</div>
+        - {{ $pulse->title }}
+    </x-slot:title>
+    <div class="max-w-2xl mx-auto py-10">
+        <a href="/"
+            class="text-sm font-bold px-4 py-2 rounded-full text-pulse-violet hover:text-white bg-pulse-petal hover:bg-pulse-rose">
+            Back to Pulses
+        </a>
+        <div class="mt-10">
+            <x-pulse-card :pulse="$pulse" full />
+        </div>
+    </div>
 </x-layout>
